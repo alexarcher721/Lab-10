48056f180ad933c710c50c0650eff3f46e743248
alexarcher721 <89867987+alexarcher721@users.noreply.github.com>
2021-12-01 22:02:42 -0600

    Update ViewUsers.php

--- ViewUsers.php
+++ ViewUsers.php
@@ -1,8 +1,10 @@
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
+$mysqli = new mysqli("cycle3.eecs.ku.edu", "a721a232", "-----", //Actual password will be used
+"a721a232");
+/* check connection */
+if ($mysqli->connect_errno) {
+printf("Connect failed: %s\n", $mysqli->connect_error); exit();
+}
 $query = "SELECT user_id FROM Users";
 $result = mysql_query($query);
 
@@ -12,5 +14,6 @@ while($row = mysql_fetch_array($result))
 echo "<tr><td>" . $row['User ID'] ."</td></tr>
 }
 echo "</table>";
+/* close connection */
+$mysqli->close();
 </body>
-</html>
