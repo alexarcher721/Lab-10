f140d95d25dbb0324d0268f8489a0570345a2fd1
alexarcher721 <89867987+alexarcher721@users.noreply.github.com>
2021-12-01 21:51:59 -0600

    Update CreatePosts.php

--- CreatePosts.php
+++ CreatePosts.php
@@ -5,7 +5,11 @@ $mysqli = new mysqli("cycle3.eecs.ku.edu", "a721a232", "-----", //Actual passwor
 if ($mysqli->connect_errno) {
 printf("Connect failed: %s\n", $mysqli->connect_error); exit();
 }
-
+$post_id = $_POST['post_id'];
+$content = $_POST['content'];
+$author_id = $_POST['author_id];
+$sql = "INSERT INTO Users (post_id,content,author_id)
+VALUES (post_id,content,author_id)";
 /* free result set */
 $result->free();
 }
