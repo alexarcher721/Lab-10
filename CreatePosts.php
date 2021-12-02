9dc0a18eb67ad6e13b600c17eee9029e9b597057
alexarcher721 <89867987+alexarcher721@users.noreply.github.com>
2021-12-01 21:51:14 -0600

    Update CreatePosts.php

--- CreatePosts.php
+++ CreatePosts.php
@@ -1,15 +1,14 @@
 <?php
-$servername = "cycle3.eecs.ku.edu";
-$username = "a721a232";
-$password = "-------"; //actual password will be used
-$dbname = "a721a232";
+$mysqli = new mysqli("cycle3.eecs.ku.edu", "a721a232", "-----", //Actual password will be used
+"a721a232");
+/* check connection */
+if ($mysqli->connect_errno) {
+printf("Connect failed: %s\n", $mysqli->connect_error); exit();
+}
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-$post_id = $_POST['post_id'];
-$content = $_POST['content'];
-$author_id = $_POST['author_id];
-$sql = "INSERT INTO Users (post_id,content,author_id)
-VALUES (post_id,content,author_id)";
-$conn->close();
+/* free result set */
+$result->free();
+}
+/* close connection */
+$mysqli->close();
 ?>
